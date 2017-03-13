359546580efa0c8c89e9d7a949b98c826d015d87
Duncan Hunter <duncan@duncanhunter.com.au>
2017-03-13 20:24:56 +1000

    add and register packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule, AuthMethods, AuthProviders } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAN7I4tEdPXG91pTr2bnt7pCrk1Or9tsMg',
+    authDomain: 'angularfire-ngsydney.firebaseapp.com',
+    databaseURL: 'https://angularfire-ngsydney.firebaseio.com',
+    storageBucket: 'angularfire-ngsydney.appspot.com',
+    messagingSenderId: '181898057682'
+  };
+
+  const firebaseAuthConfig = {
+    providers: AuthProviders.Anonymous,
+    methods: AuthMethods.Anonymous
+  };
 
 @NgModule({
   declarations: [
@@ -12,7 +25,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
