f060f7ac7baacd2e9f954f0b2f61dd833fcc2303
Duncan Hunter <duncan@duncanhunter.com.au>
2017-03-15 17:59:39 +1100

    working

--- functions/index.js
+++ functions/index.js
@@ -4,16 +4,24 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.countVotes = functions.database.ref('/votes')
-    .onWrite(function (event) {
-    // if (event.data.previous.exists()) {
-    //   return;
-    // }
-    var results = { total: 0, v4: 0, v2: 0 };
+  .onWrite(function (event) {
+    
+    if (event.data.previous.exists()) {
+      return;
+    }
+
+    var results = {
+      total: 0,
+      v4: 0,
+      v2: 0
+    };
+
     var votes = event.data._newData;
-    Object.keys(votes).map(function (key) {
-        console.log('key1', key);
-        results.total++;
-        votes[key] === 'v4' ? results.v4++ : results.v2++;
+    Object.keys(votes).map(function(key) {
+      console.log('key1', key);
+      results.total++;
+      votes[key] === 'v4' ? results.v4++ : results.v2++;
     });
+
     return admin.database().ref('/voteCount').set(results);
-});
+  });
